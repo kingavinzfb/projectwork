ce07703774183f89e35ed1c45dfefece1040adf0
aca17fz <fzeng5@sheffield.ac.uk>
2021-09-16 05:11:19 +0100

    new

---
+++ pac-man/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/passoni/Pac-man/src
+# Build directory: /home/passoni/Pac-man/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("tb3")
